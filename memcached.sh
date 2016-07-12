0d57d7e1c1b6f0193d363f429444f0e7900b00b5
nuxsky <nuxsky@sina.com>
2016-07-12 11:03:00 +0800

    memcached.sh v1.0.1

--- memcached.sh
+++ memcached.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
-# v1.0.0
+# v1.0.1
 
 meminit=/etc/init.d/memcached
-if [ -n $1 ]; then
+memdir=/usr/local/memcached
+if [ ! $1 ]; then
   ipaddr=127.0.0.1
 else
   ipaddr=`ifconfig | egrep 'inet addr:192|inet addr:10.|inet addr:172.' | head -1 | cut -d: -f2 | cut -d' ' -f1`
@@ -16,15 +17,20 @@ if [ ! -d /usr/local/memcached ]; then
   wget http://pkg.nuxsky.com/memcached-1.4.25.tar.gz
   tar zxf memcached-1.4.25.tar.gz
   cd memcached-1.4.25
-  ./configure --prefix=/usr/local/memcached --enable-64bit --enable-threads
+  ./configure --prefix=$memdir --enable-64bit --enable-threads
   make && make install
+  mkdir $memdir/run
 fi
 
-cp -f scripts/memcached.sysv $meminit
+cp -f $memdir/scripts/memcached.sysv $meminit
 sed -i 's@PORT=11211@PORT=11233@g' $meminit
 sed -i 's@USER=nobody@USER=memcached@g' $meminit
 sed -i 's@CACHESIZE=64@CACHESIZE=2048@g' $meminit
-sed -i "s@OPTIONS=\"\"@OPTIONS=\"-l $ipaddr\"@g' $meminit
+sed -i "s@OPTIONS=\"\"@OPTIONS=\"-l $ipaddr\"@g" $meminit
+sed -i "s@daemon memcached@daemon $memdir/bin/memcached@g" $meminit
+sed -i 's@chown@# chown@g' $meminit
+sed -i "s@/var/run/memcached/@$memdir/run/@g" $meminit
+sed -i "s@/var/run/memcached.pid@$memdir/run/memcached.pid@g" $meminit
 chmod 755 $meminit
 chkconfig ${meminit##*/} on
 $meminit start
