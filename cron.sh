2d9f99bf97ad956e5db7390682fd20fe0a38b59e
nuxsky <nuxsky@sina.com>
2016-07-18 13:48:14 +0800

    cron.sh v1.0.1

--- cron.sh
+++ cron.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# v1.0.0
+# v1.0.1
 
 profun() {
 	if [ $1 = 'ecstore' ] || [ $1 = 'bbc' ]; then
@@ -19,9 +19,9 @@ profun() {
 	
 }
 bkupsite() {
-	echo "/bin/tar zcf /data/backup/data/$1-"'$gz'" /data/www/$1 --exclude=/data/www/$1/data --exclude=/data/www/$1/public" >> $bkupsh
-	echo "/usr/local/mysql/bin/mysqldump -h 127.0.0.1 -u$1 -p'123456' db_$1 > /data/backup/data/$1-"'$File' >> $bkupsh
-	echo  "" >> $bkupsh
+	dbpass=`grep $1 sysconfig.log | cut -d' ' -f3`
+	echo "/bin/tar zcf /data/backup/data/$1-\$gz /data/www/$1 --exclude=/data/www/$1/data --exclude=/data/www/$1/public" >> $bkupsh
+	echo -e "/usr/local/mysql/bin/mysqldump -h 127.0.0.1 -u$1 -p'$dbpass' db_$1 > /data/backup/data/$1-\$File\n" >> $bkupsh
 }
 
 if [ ! -d /data/backup ]; then
@@ -32,7 +32,7 @@ cronfile=/tmp/cronfile
 bkupsh=/data/backup/backup.sh
 scmsh=/data/backup/shopex-collect-mem.sh
 :> $cronfile
-echo -e '#!/bin/bash\nNow=$(date +"%d-%m-%Y--%H:%M:%S")\nFile=backup-$Now.sql\ngz=backup-$Now.tar.gz\n' > $bkupsh
+echo -e "#!/bin/bash\nNow=\$(date +\"%d-%m-%Y--%H:%M:%S\")\nFile=backup-\$Now.sql\ngz=backup-\$Now.tar.gz\n" > $bkupsh
 
 for dir in `/bin/ls -la /data/www/ | grep "^d" | awk '{print $9}'`
 do
@@ -43,7 +43,7 @@ do
 	;;
 	bbc)
 		profun bbc 54
-		bkupsite bbs
+		bkupsite bbc
 	;;
 	oms)
 		profun oms
@@ -57,7 +57,7 @@ do
 done
 
 echo 'find /data/backup/data/ -mtime +10 -exec rm -f {} \;' >> $bkupsh
-echo -e '#/bin/bash\n##回收php-fpm内存\n###made by shopex.cn\nKILLPID=$(ps aux|grep "php-fpm: pool"|grep -v grep |awk '"'{if(\$4>=1)print \$2}'"')\nDATE=$(date +%F-%T)\necho $KILLPID\nfor PIDS in $KILLPID\ndo\necho $DATE  $PIDS >>/tmp/killpid.logs\nkill -15 $PIDS\ndone' > $scmsh
+echo -e "#/bin/bash\nKILLPID=\$(ps aux|grep \"php-fpm: pool\"|grep -v grep |awk '{if(\$4>=1)print \$2}')\nDATE=\$(date +%F-%T)\necho \$KILLPID\nfor PIDS in \$KILLPID\ndo\necho \$DATE  \$PIDS >>/tmp/killpid.logs\nkill -15 \$PIDS\ndone" > $scmsh
 echo '30 3 * * * sh /data/backup/backup.sh' > ${cronfile}.root
 echo '20 */3 * * * sh /data/backup/shopex-collect-mem.sh' >> ${cronfile}.root
 
