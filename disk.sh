23a55a075154354d41c16030b4b9b2f351866834
nuxsky <nuxsky@sina.com>
2016-08-18 18:11:52 +0800

    创建data磁盘卷，挂载到/data。从安全方面考虑，未设置成自动获取磁盘名称，需手动输入（如：xvdb）

--- disk.sh
+++ disk.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 # v1.0.0
-# 创建data磁盘卷，挂载到/data。从安全方面考虑，需要手动输入硬盘名称（如：xvdb）
+# 创建data磁盘卷，挂载到/data。从安全方面考虑，未设置成自动获取磁盘名称，需手动输入（如：xvdb）
 
 [ ! $1 ] && exit 1
 DISKSIZE=(lsblk | grep $1 | sed -n '1p' | cut -d 'G' -f1 |  awk '{if($4<=200) {print $4-2} else {print $4-5}}')
