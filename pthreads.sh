8d47557538d0a9a9a20f8535f97dbea8475b1838
nuxsky <nuxsky@sina.com>
2016-08-18 13:57:52 +0800

    pthreads v1.0.1

--- pthreads.sh
+++ pthreads.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-# 安装php-pthreads v1.0.0
+# 安装php-pthreads v1.0.1
 
 [ ! -f /etc/yum.repos.d/shopex-lnmp.repo ] && wget mirrors.shopex.cn/shopex/shopex-lnmp/shopex-lnmp.repo -P /etc/yum.repos.d/ && yum clean metaddata && yum makecache
-[ ! -d /usr/local/pthreads ] && yum install php-pthreads.x86_64 -y
+[ ! -d /usr/local/php-pthreads ] && yum install php-pthreads.x86_64 -y
