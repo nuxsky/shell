db5e23e7eae308ac0fda3a95de6f2256b6727c37
nuxsky <nuxsky@sina.com>
2016-08-19 09:41:30 +0800

    修复错误

--- disk.sh
+++ disk.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
-# v1.0.1
+# v1.0.2
 # 创建data磁盘卷，挂载到/data。从安全方面考虑，未设置成自动获取磁盘名称，需手动输入（如：xvdb）
 
 [ ! $1 ] && [[ $1 =~ 'a' ]] && exit 1
-DISKSIZE=(lsblk | grep $1 | sed -n '1p' | cut -d 'G' -f1 |  awk '{if($4<=200) {print $4-2} else {print $4-5}}')
+DISKSIZE=$(lsblk | grep $1 | sed -n '1p' | cut -d 'G' -f1 |  awk '{if($4<=200) {print $4-2} else {print $4-5}}')
 pvcreate /dev/$1
 vgcreate vg_data /dev/$1
 lvcreate -n lv_data -L ${DISKSIZE}G vg_data
