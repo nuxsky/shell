a79806a9cf3f14ad70a3de71f3d567fb641a132e
nuxsky <nuxsky@sina.com>
2016-08-22 21:31:17 +0800

    修复错误

--- logrotate.sh
+++ logrotate.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
-# v1.0.0
+# v1.0.1
 # logrotate 日志文件分割
 
 [ $1 ] || exit 1
 for prg in $@
 do
-	[ -f /etc/logrotate.d/$prg ] || wget http://cfg.nuxsky.com/logrotate/$prg -O /etc/logrotate/$prg
+	[ -f /etc/logrotate.d/$prg ] || wget http://cfg.nuxsky.com/logrotate/$prg -O /etc/logrotate.d/$prg
 done
