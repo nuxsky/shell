3d26cbe0f6d7455548d1a64cc06fe30e8977fdd3
nuxsky <nuxsky@sina.com>
2016-08-22 18:11:15 +0800

    精简代码、优化功能

--- redis.sh
+++ redis.sh
@@ -1,28 +1,24 @@
 #!/bin/sh
-# v1.0.3
+# v1.0.4
+# redis安装脚本
 
-case $1 in
-kv)
-  port=6380
-  rdsinit=/etc/init.d/redis_kv
-  ipaddr=`ifconfig | egrep 'inet addr:192|inet addr:10.|inet addr:172.' | head -1 | cut -d: -f2 | cut -d' ' -f1`
-  logfile=/data/logs/redis/redis_kv.log
-  ;;
-qu)
-  port=6390
-  rdsinit=/etc/init.d/redis_qu
-  ipaddr=`ifconfig | egrep 'inet addr:192|inet addr:10.|inet addr:172.' | head -1 | cut -d: -f2 | cut -d' ' -f1`
-  logfile=/data/logs/redis/redis_qu.log
-  ;;
-*)
-  port=6379
-  rdsinit=/etc/init.d/redis
-  ipaddr=127.0.0.1
-  logfile=/data/logs/redis/redis.log
-  ;;
-esac
+[ $1 ] && port=$1 && rdser=_$port || port=6379
+rdsinit=/etc/init.d/redis$rdser
+logfile=/data/logs/redis/redis$rdser.log
 rdsdir=/usr/local/redis
-rdsconf=${rdsdir}/conf/${port}.conf
+rdsconf=${rdsdir}/conf/$port.conf
+
+case ${!#} in
+wan)
+	ipaddr=$(ifconfig | grep 'inet ' | grep -v '127.0.0.1' | cut -d':' -f2 | cut -d' ' -f1 | egrep -v '^(10.|172.|192.)')
+	;;
+lan)
+	ipaddr=$(ifconfig | grep 'inet ' | grep -v '127.0.0.1' | cut -d':' -f2 | cut -d' ' -f1 | egrep '^(10.|172.|192.)')
+	;;
+*)
+	ipaddr=127.0.0.1
+	;;
+esac
 
 if [ ! -d /usr/local/redis ];then
   cd /usr/local/src
@@ -36,13 +32,13 @@ if [ ! -d /usr/local/redis ];then
 fi
 
 cp -f ${rdsdir}/utils/redis_init_script $rdsinit
-cp -f ${rdsdir}/redis.conf ${rdsdir}/conf/$port.conf
 sed -i 's@#$@# chkconfig: 2345 88 12@g' $rdsinit
 sed -i "s@REDISPORT=6379@REDISPORT=$port@g" $rdsinit
 sed -i "s@/usr/local/bin@${rdsdir}/src@g" $rdsinit
 sed -i "s@shutdown\$@-h $ipaddr shutdown@g" $rdsinit
 sed -i "s@/var/run/redis_@${rdsdir}/run/@g" $rdsinit
 sed -i "s@/etc/redis@${rdsdir}/conf@g" $rdsinit
+cp -f ${rdsdir}/redis.conf $rdsconf
 sed -i 's@daemonize no@daemonize yes@g' $rdsconf
 sed -i "s@port 6379@port $port@g" $rdsconf
 sed -i "s@pidfile /var/run/redis.pid@pidfile ${rdsdir}/run/${port}.pid@g" $rdsconf
