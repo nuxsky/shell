471984e15abf3461b0ffb95bb4c6cd4240e6067f
nuxsky <nuxsky@sina.com>
2016-08-23 11:36:13 +0800

    优化代码

--- install.sh
+++ install.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 # v1.0.3
 
-site=http://sh.nuxsky.com
-
 if [ $1 ]; then
-  wget $site/$1.sh
+  wget http://sh.nuxsky.com/$1.sh
   sh $1.sh ${@:2}
   rm -f $1.sh
   echo "$1.sh ${@:2}" >> install.log
