f3829095eb1d41554301d45f18bdd330c617f310
nuxsky <nuxsky@sina.com>
2016-08-23 13:29:13 +0800

    优化代码

--- dbs.sh
+++ dbs.sh
@@ -1,37 +1,23 @@
 #!/bin/bash
-# v1.0.3
+# v1.0.4
+# 创建数据库
 
-sclog=/usr/local/src/sysconfig.log
 sqlfile=/tmp/sqlfile
+sclog=/usr/local/src/sysconfig.log
+[ $1 ] && site=$@ || site=$(ls /data/www -F | grep '/$' | cut -d'/' -f1)
 echo "mysql:" > $sclog
 :> $sqlfile
 
 sqldata() {
-    str=`head -c 500 /dev/urandom | tr -dc 0-9a-zA-Z`
-    str=${str:0:16}
-    echo -e "drop database if exists db_$1;\ncreate database db_$1 default character set utf8;\ngrant all on db_${1}.* to $1@'%' identified by '$str';\nflush privileges;"  >> $sqlfile
-    echo "db_$1 $1 $str" >> $sclog
+    str=$(head -c 500 /dev/urandom | tr -dc 0-9a-zA-Z)
+    echo -e "drop database if exists db_$1;\ncreate database db_$1 default character set utf8;\ngrant all on db_${1}.* to $1@'%' identified by '${str:0:16}';\nflush privileges;"  >> $sqlfile
+    echo "db_$1 $1 ${str:0:16}" >> $sclog
 }
 
-if [ $1 ]; then sqldata $1; else
-    for dir in `/bin/ls -la /data/www/ | grep "^d" | awk '{print $9}'`
-    do
-        case $dir in
-        ecstore)
-            sqldata ecstore
-        ;;
-        bbc)
-            sqldata bbc
-        ;;
-        oms)
-            sqldata oms
-        ;;
-        crm)
-            sqldata crm
-        ;;
-        esac
-    done
-fi
+for dir in $site
+do
+	sqldata $dir
+done
 
 /usr/local/mysql/bin/mysql -uroot --password='' < $sqlfile
 rm -f $sqlfile
