110392b65e0993e9242946b0193cfe081fb16c43
nuxsky <nuxsky@sina.com>
2016-11-26 11:50:46 +0800

    1

--- jdk.sh
+++ jdk.sh
@@ -3,7 +3,7 @@
 # 安装jdk
 
 wget http://dl.nuxsky.com/jdk-8u101-linux-x64.tar.gz
-tar zxf jdk-8u101-linux-x64.gz -C /usr/local
+tar zxf jdk-8u101-linux-x64.tar.gz -C /usr/local
 rm -f jdk-8u101-linux-x64.tar.gz
 echo -e '\nexport JAVA_HOME=/usr/local/jdk1.8.0_101\nexport PATH=$JAVA_HOME/bin:$PATH' >> /etc/profile
 . /etc/profile
