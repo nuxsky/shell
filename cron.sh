f1937d037b44954e7711e267b41c3b457724872a
nuxsky <nuxsky@sina.com>
2016-12-28 10:33:04 +0800

    11

--- cron.sh
+++ cron.sh
@@ -9,7 +9,7 @@ do
 	case $dir in
 	*ecstore|*bbc)
 		[ $dir = 'bbc' ] && pvs='54'
-		[[ $(crontab -uwww -l) =~ $dir ]] || echo -e "* * * * * /usr/local/php$pvs/bin/php /data/www/$dir/script/queue/queue.sh >/dev/null\n* * * * * /usr/local/php$pvs/bin/php /data/www/$dir/script/crontab/crontab.php >/dev/null" >> /var/spool/cron/www
+		[[ $(crontab -uwww -l) =~ $dir ]] || echo -e "* * * * * /data/www/$dir/script/queue/queue.sh /usr/local/php$pvs/bin/php >/dev/null\n* * * * * /usr/local/php$pvs/bin/php /data/www/$dir/script/crontab/crontab.php >/dev/null" >> /var/spool/cron/www
 		;;
 	*oms)
 		[[ $(crontab -uwww -l) =~ $dir ]] || echo -e "* * * * * /bin/sh /data/www/$dir/app/taskmgr/check.sh\n0 0 * * * /bin/sh /data/www/$dir/app/taskmgr/cleanlogs.sh" >> /var/spool/cron/www
