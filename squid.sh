f525301fef0caf4846c1b110db4dfc148446ba8d
nuxsky <nuxsky@sina.com>
2017-07-11 11:48:21 +0800

    123

--- squid.sh
+++ squid.sh
@@ -8,8 +8,8 @@ cd squid-3.5.26
 useradd -M -s /sbin/nologin squid
 ./configure --prefix=/usr/local/squid --localstatedir=/data/squid --enable-gnuregex --enable-async-io=240 --enable-icmp --enable-snmp --enable-cache-digests --enable-default-err-language="Simplify_Chinese" --enable-linux-netfiter --enable-delay-pools --with-filedescriptors=65536  --disable-loadable-modules
 make && make install
-wget http://cfg.nuxsky.com/squid/squid -O /etc/init.d/squid && chmod +x /etc/init.d/squid
-wget http://cfg.nuxsky.com/squid/squid.conf -O /usr/local/squid/etc/squid.conf
+wget http://xxx.com/squid/squid -O /etc/init.d/squid && chmod +x /etc/init.d/squid
+wget http://xxx.com/squid/squid.conf -O /usr/local/squid/etc/squid.conf
 chown squid.squid -R /data/squid
 /usr/local/squid/sbin/squid -z
 chkconfig squid on
